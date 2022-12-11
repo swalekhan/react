a81601cdd2889f3438c648aaa41e5a91ecdd0d93
swalekhan <swalekhan1522@gmail.com>
2022-12-11 20:51:09 +0530

    basic component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from "./component/ExpenseItem"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let expense = [
+    { title: "trip", amount: "10", date: new Date() },
+    { title: "shoping", amount: "100", date: new Date() },
+    { title: "food", amount: "50", date: new Date() }
+  ];
+    return (
+      <div >
+         <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date} />
+         <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date} />
+         <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date} />
+      </div>
+    );
 }
-
 export default App;
