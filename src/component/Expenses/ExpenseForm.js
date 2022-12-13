869d27df1a87e2e5beeb69b2e604259e06e27707
swalekhan <swalekhan1522@gmail.com>
2022-12-13 20:15:38 +0530

    form

--- src/component/Expenses/ExpenseForm.js
+++ src/component/Expenses/ExpenseForm.js
@@ -1,26 +1,48 @@
+import { useState } from 'react'
 import './ExpenseForm.css'
 const ExpenseForm = () =>{
-    const changeHandler =(e)=>{
+    const [title, setTitle] = useState("ExpenseTitle")
+    const [amount, setAmount] = useState("ExpenseAmount")
+    const [date, setDate] = useState("ExpenseDate")
+
+    const titleHandler =(e)=>{
+        setTitle(e.target.value)
+    }
+    const amountHandler =(e)=>{
+        setAmount(e.target.value)
+    }
+    const dateHandler =(e)=>{
+        setDate(e.target.value)
         console.log(e.target.value)
     }
 
+    const clickHendler = (e)=>{
+        e.preventDefault()
+      let setShow ={
+        t:title,
+        a:amount,
+        d:date
+       }
+       console.log(setShow)
+    }
+ 
     return (
         <div className="outer-div">
             <div className="inner-div">
-            <form>
+            <form onSubmit={clickHendler}>
                 <p>
-                <label>ExpenseTitle</label><br/>
-                <input type="text" onChange={changeHandler}/><br/>
+                <label>{title}</label><br/>
+                <input type="text" onChange={titleHandler}/><br/>
                 </p>
                 <p>
-                <label>ExpenseTitle</label><br/>
-                <input type="text" onChange={changeHandler}/><br/>
+                <label>{amount}</label><br/>
+                <input type="number" onChange={amountHandler}/><br/>
                 </p>
                 <p>
-                <label>ExpenseTitle</label><br/>
-                <input type="text" onChange={changeHandler}/><br/>
+                <label>{date}</label><br/>
+                <input type="date" onChange={dateHandler}/><br/>
                 </p>
-                <input type="submit"/>
+                <input type="submit" />
             </form>
             </div>
         </div>
